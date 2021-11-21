ec821fbdf967fef51139070c3fbb169978aa8595
Oscar Rovira <cruzrovira@gmail.com>
2021-11-20 21:56:06 -0600

    Âadd middleware

--- src/index.js
+++ src/index.js
@@ -4,84 +4,19 @@ const app = express()
 app.use(express.json())
 require("./services/mongoose")
 
-const Note = require("./models/note")
+const noteMiddleware = require("./middleware/noteMiddleware")
 
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
 
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes)
-  })
-})
+app.use("/api/notes", noteMiddleware)
 
-app.get("/api/notes/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const note = await Note.findById(id)
-    if (note) {
-      res.json(note)
-    } else {
-      res.status(404).send({ error: `Note with id ${id} not found` })
-    }
-  } catch (error) {
-    res.status(400).send(error)
-  }
-})
-
-app.delete("/api/notes/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const note = await Note.findByIdAndRemove(id)
-    if (note) {
-      res.status(204).end()
-    } else {
-      res.status(404).send({ error: `Note with id ${id} not found` })
-    }
-  } catch (error) {
-    res.status(400).send(error)
-  }
-})
-
-app.post("/api/notes", async (req, res) => {
-  const body = req.body
-
-  if (!body.content) {
-    return res.status(400).json({ error: "content missing" })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  try {
-    const savedNote = await note.save()
-    res.json(savedNote)
-  } catch (error) {
-    res.status(400).send(error)
-  }
-})
-
-app.put("/api/notes/:id", async (req, res) => {
-  const id = req.params.id
-
-  const { content, important } = req.body
-  const note = {
-    content: content,
-    important: important,
-  }
-  try {
-    const updatedNote = await Note.findByIdAndUpdate(id, note, { new: true })
-    res.json(updatedNote)
-  } catch (error) {
-    res.status(400).send(error)
-  }
+app.use((req, res) => {
+  res.status(404).send({ error: "Not found" })
 })
 
 const port = process.env.PORT || 3000
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
---
+++ src/middleware/noteMiddleware.js
@@ -0,0 +1,84 @@
+const noteMiddleware = require("express").Router()
+const Note = require("../models/note")
+const mongose = require("mongoose")
+
+noteMiddleware.get("", (req, res) => {
+  Note.find({}).then((notes) => {
+    mongose.connection.close()
+    res.json(notes)
+  })
+})
+
+noteMiddleware.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const note = await Note.findById(id)
+
+    if (note) {
+      res.json(note)
+    } else {
+      res.status(404).send({ error: `Note with id ${id} not found` })
+    }
+  } catch (error) {
+    res.status(400).send(error)
+  } finally {
+    mongose.connection.close()
+  }
+})
+
+noteMiddleware.delete("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const note = await Note.findByIdAndRemove(id)
+    if (note) {
+      res.status(204).end()
+    } else {
+      res.status(404).send({ error: `Note with id ${id} not found` })
+    }
+  } catch (error) {
+    res.status(400).send(error)
+  } finally {
+    mongose.connection.close()
+  }
+})
+
+noteMiddleware.post("", async (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  try {
+    const savedNote = await note.save()
+    res.json(savedNote)
+  } catch (error) {
+    res.status(400).send(error)
+  } finally {
+    mongose.connection.close()
+  }
+})
+
+noteMiddleware.put("/:id", async (req, res) => {
+  const id = req.params.id
+
+  const { content, important } = req.body
+  const note = {
+    content: content,
+    important: important,
+  }
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(id, note, { new: true })
+    res.json(updatedNote)
+  } catch (error) {
+    res.status(400).send(error)
+  } finally {
+    mongose.connection.close()
+  }
+})
+module.exports = noteMiddleware
